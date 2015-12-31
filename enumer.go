54304668d74c23f1a1c28b43c10b3056bffbd576
alvaroloes <alvarolopezespinosa@gmail.com>
2015-12-31 14:03:36 +0000

    Move code out of the inner for loop as it only depend on the outer one

--- enumer.go
+++ enumer.go
@@ -15,21 +15,19 @@ func (g *Generator) buildValueToNameMap(runs [][]Value, typeName string, runsThr
 	// At this moment, either "g.declareIndexAndNameVars()" or "g.declareNameVars()" has been called
 	g.Printf("\nvar _%sNameToValue_map = map[string]%s{\n", typeName, typeName)
 	thereAreRuns := len(runs) > 1 && len(runs) <= runsThreshold
-	n := 0
+	var n int
 	var runID string
 	for i, values := range runs {
-		for _, value := range values {
-			if thereAreRuns {
-				runID = "_" + fmt.Sprintf("%d",i)
-			} else {
-				runID = ""
-			}
+		if thereAreRuns {
+			runID = "_" + fmt.Sprintf("%d",i)
+			n = 0
+		} else {
+			runID = ""
+		}
 
+		for _, value := range values {
 			g.Printf("\t_%s_name%s[%d:%d]: %s,\n", typeName, runID, n, n+len(value.name), &value)
 			n += len(value.name)
-		}
-		if thereAreRuns {
-			n = 0
 		}
 	}
 	g.Printf("}\n\n")
