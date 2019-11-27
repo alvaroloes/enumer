346a688fa0278bf13e65d904c1d04579a385f1a4
Aurélien Bec <aurelien.bec@gmail.com>
2019-11-27 14:12:41 +0100

    Add test case for -transform=lowercamel

--- endtoend_test.go
+++ endtoend_test.go
@@ -64,6 +64,9 @@ func TestEndToEnd(t *testing.T) {
 		if name == "transform.go" {
 			typeName = "CamelCaseValue"
 			transformNameMethod = "snake"
+		} else if name == "transformlowercamel.go" {
+			typeName = "CamelCaseValue"
+			transformNameMethod = "lowercamel"
 		}
 
 		stringerCompileAndRun(t, dir, stringer, typeName, name, transformNameMethod)
